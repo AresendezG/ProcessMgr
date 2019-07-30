3c001a57343d056d4610fcf8ed0de6b549c288e2
Alejandro Resendez <al.resendez.gro@gmail.com>
2019-07-30 10:09:34 -0500

    Add option to close processes

--- Program.cs
+++ Program.cs
@@ -35,6 +35,47 @@ namespace process_manager
                         Console.WriteLine("Process {0} is Running", process_name);
                 }
 
+                if (option == "kill")
+                {
+                    string process_name = args[1];
+                    Process[] localByName = Process.GetProcessesByName(process_name);
+                    
+                    try
+                    {
+                        localByName[0].Kill();
+                        Console.WriteLine("Successfully Closed {0} process", process_name);
+                    }
+
+                    /*
+                     Win32Exception
+                              The associated process could not be terminated.
+                              The process is terminating.
+
+                     NotSupportedException
+                              You are attempting to call Kill() for a process that is running on a remote computer. The method is available only for processes running on the local computer.
+
+                    InvalidOperationException
+                     
+                     */
+
+                    catch (Win32Exception e)
+                    {
+                        Console.WriteLine("Access Denied for terminating this process: ");
+                        Console.WriteLine(e.Message);
+                    }
+                    catch (NotSupportedException e)
+                    {
+                        Console.WriteLine("You can't terminate remote process: ");
+                        Console.WriteLine(e.Message);
+                    }
+                    catch (InvalidOperationException e)
+                    {
+                        Console.WriteLine("This process cannot be terminated: ");
+                        Console.WriteLine(e.Message);
+                    }
+                }
+
+
                 if (option  == "run")
                 {
                     string process_path = args[1]; //Second passing argument is the path
